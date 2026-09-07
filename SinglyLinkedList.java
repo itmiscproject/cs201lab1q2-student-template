8c1e2de15fabd690ecbad0f62bb51e7d0eb55f66
cookiemo729 <58022041+cookiemo729@users.noreply.github.com>
2026-09-07 16:30:14 +0800

    Meraaj

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,48 +1,47 @@
-import java.util.*;
+public class SinglyLinkedList<E> {
 
-public class SinglyLinkedList<E extends Comparable<E>> {
     private Node<E> head = null;
     private Node<E> tail = null;
     private int size = 0;
 
-    public SinglyLinkedList(){
+    public SinglyLinkedList() {
 
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public E first(){
-        if (isEmpty()){
+    public E first() {
+        if (isEmpty()) {
             return null;
-        } 
+        }
         return head.getElement();
     }
 
-    public E last(){
-        if (isEmpty()){
+    public E last() {
+        if (isEmpty()) {
             return null;
         }
         return tail.getElement();
     }
 
-    public void addFirst(E e){
+    public void addFirst(E e) {
         head = new Node<>(e, head);
 
-        if (isEmpty()){
+        if (isEmpty()) {
             tail = head;
         }
         size++;
     }
 
-    public void addLast(E e){
+    public void addLast(E e) {
         Node<E> newest = new Node<>(e, null);
-        if (isEmpty()){
+        if (isEmpty()) {
             head = newest;
         } else {
             tail.setNext(newest);
@@ -51,8 +50,8 @@ public class SinglyLinkedList<E extends Comparable<E>> {
         size++;
     }
 
-    public E removeFirst(){
-        if (isEmpty()){
+    public E removeFirst() {
+        if (isEmpty()) {
             return null;
         }
 
@@ -60,63 +59,67 @@ public class SinglyLinkedList<E extends Comparable<E>> {
         head = head.getNext();
         size--;
 
-        if (isEmpty()){
+        if (isEmpty()) {
             tail = null;
         }
         return answer;
     }
 
-    /*************************************/
-    public String toString(){
-        String result = "";
-        if (isEmpty()){
-            return result;
-        }
+    // Write your codes below
+    public String toString() {
+        StringBuilder result = new StringBuilder();
 
         Node<E> current = head;
-        while (current != null){
-            result += current.getElement();
+
+        while (current != null) {
+            result.append(current.getElement());
             current = current.getNext();
         }
-        return result;
+
+        return result.toString();
     }
 
-    public E removeLast(){
-        if (isEmpty()){
+    public E removeLast() {
+        if (isEmpty()) {
             return null;
         }
 
-        Node<E> last = tail;
-        
-        if (head == tail){
+        E answer = tail.getElement();
+
+        if (size == 1) {
             head = null;
             tail = null;
-            size--;
-            return last.getElement();
+            size = 0;
+            return answer;
         }
 
         Node<E> current = head;
-        while (current.getNext() != tail){
+
+        while (current.getNext() != tail) {
             current = current.getNext();
         }
+
         current.setNext(null);
         tail = current;
         size--;
-        return last.getElement();
+        return answer;
     }
 
-    public void reverse(){      
-        Node<E> previous = null; 
-        Node<E> current = head; 
-        Node<E> next = null; 
-        
-        tail = head; 
+    public void reverse() {
+        Node<E> previous = null;
+        Node<E> current = head;
+
+        tail = head;
+
         while (current != null) {
-			next = current.getNext(); 
-			current.setNext(previous); 
-			previous = current; 
-			current = next; 
+            Node<E> next = current.getNext();
+
+            current.setNext(previous);
+
+            previous = current;
+            current = next;
         }
-        head = previous; 
+
+        head = previous;
     }
 }
